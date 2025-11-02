de88214f6998949d3b946e9f32d4dc04600501ee
Prasang Mani Manav <moneymanav9211@gmail.com>
2025-11-02 20:11:25 +0530

    implemented LRU cache

--- cache/cache.go
+++ cache/cache.go
@@ -9,39 +9,95 @@ import (
 // in this cache I am not handling the collision for simplicity
 // Using LRU for eviction policy
 type Cache struct {
-	data map[string]*models.CountryMetadata
-	dll  *Node
+	data map[string]*Node
+	dll  *DoublyLinkedList
 	size int
 	cap  int
 }
 
 func NewCache(capacity int) *Cache {
 	return &Cache{
-		data: make(map[string]*models.CountryMetadata),
+		data: make(map[string]*Node),
 		cap:  capacity,
-		dll:  &Node{},
+		dll:  &DoublyLinkedList{},
 	}
 }
 
-func (cache *Cache) Set(key string, value models.CountryMetadata) {
-	cache.data[key] = &value
+func (cache *Cache) Set(key string, value *models.CountryMetadata) {
+	if node, exists := cache.data[key]; exists {
+		node.value = value
+		// move existing node to front (no size change)
+		if cache.dll.head != node {
+			cache.dll.remove(node)
+			cache.dll.addToFront(node)
+		}
+		return
+	}
+	cache.data[key] = NewNode(key, value)
+	cache.dll.addToFront(cache.data[key])
+	cache.size++
+	if cache.size > cache.cap {
+		cache.removeOldest()
+	}
+}
 
+func (cache *Cache) removeOldest() {
+	if cache.dll.tail != nil {
+		delete(cache.data, cache.dll.tail.key)
+		cache.dll.remove(cache.dll.tail)
+		cache.size--
+	}
 }
 
 func (cache *Cache) Get(key string) (models.CountryMetadata, error) {
 	if cache.data[key] == nil {
 		return models.CountryMetadata{}, fmt.Errorf("key not found")
 	}
-	return *cache.data[key], nil
+	return *cache.data[key].value, nil
 }
 
-func remove() {
+type DoublyLinkedList struct {
+	head *Node
+	tail *Node
+}
 
+func (dll *DoublyLinkedList) addToFront(node *Node) {
+	node.prev = nil
+	node.next = dll.head
+	if dll.head != nil {
+		dll.head.prev = node
+	}
+	dll.head = node
+	if dll.tail == nil {
+		dll.tail = node
+	}
+}
+
+func (dll *DoublyLinkedList) remove(node *Node) {
+	if node.prev != nil {
+		node.prev.next = node.next
+	} else {
+		dll.head = node.next
+	}
+	if node.next != nil {
+		node.next.prev = node.prev
+	} else {
+		dll.tail = node.prev
+	}
 }
 
 type Node struct {
 	key   string
-	value models.CountryMetadata
+	value *models.CountryMetadata
 	prev  *Node
 	next  *Node
+}
+
+func NewNode(key string, value *models.CountryMetadata) *Node {
+	return &Node{
+		key:   key,
+		value: value,
+		prev:  nil,
+		next:  nil,
+	}
 }
